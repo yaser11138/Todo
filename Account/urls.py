a6c0fa3bdf16f9e0f0e0222d53074c01f78db70b
yaser <ydahgan@gmail.com>
2022-05-02 11:16:56 +0430

    add logout

--- Account/urls.py
+++ Account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import Register
+from .views import Register, Logout
 urlpatterns =[
     path("login/", obtain_auth_token, name="login"),
     path("register/", Register.as_view(), name="register"),
+    path("logout/", Logout.as_view(), name="logout")
 ]
--- Account/views.py
+++ Account/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializers
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
 
 class Register(APIView):
@@ -12,8 +14,18 @@ class Register(APIView):
         user_data = UserSerializers(data=request.data)
         if user_data.is_valid():
             user_data.save()
-            return Response(data={"user": user_data.data})
+            return Response(data={"user": user_data.data}, status=status.HTTP_201_CREATED)
         else:
-            return Response(data={"error": user_data.errors})
+            return Response(data={"error": user_data.errors}, status=status.HTTP_400_BAD_REQUEST)
 
+
+class Logout(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(
+            data={'message': f'Bye {request.user.username}!'},
+            status=status.HTTP_204_NO_CONTENT
+        )
 
